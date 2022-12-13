076580fc2de7cdf2dc2a3dcf20fc051b4f259d45
Randy Velasco <rbvelasco@eastwestbanker.com>
2022-12-13 15:14:41 +0800

    add marketplace on routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,23 +3,13 @@ import { Route, Routes } from "react-router";
 import { BrowserRouter as Router, Navigate } from "react-router-dom";
 import NavMenu from "components/navMenu";
 import Layout from "components/Layout";
-<<<<<<< HEAD
-import Home from "pages/Home";
-import Settings from "pages/Settings";
-import Notification from "pages/Notification";
-import Profile from "pages/Profile";
-import Marketplace from "pages/Marketplace";
-import { MARKETPLACE_PATH,NOTIFICATIONS_PATH, PROFILE_PATH } from "./constants";
-
-=======
-import { NOTIFICATIONS_PATH, PROFILE_PATH } from "./constants";
->>>>>>> 9752f7294776129a51571137cc0cf33a244f0c8e
+import { NOTIFICATIONS_PATH, PROFILE_PATH ,MARKETPLACE_PATH } from "./constants";
 
 const Home = lazy(() => import('pages/Home'));
 const Settings = lazy(() => import('pages/Settings'));
 const Notification = lazy(() => import('pages/Notification'));
 const Profile = lazy(() => import('pages/Profile'));
-
+const Marketplace = lazy(() => import('pages/Marketplace'));
 
 
 const RootRoute = () => {
@@ -28,28 +18,17 @@ const RootRoute = () => {
       <Router>
         <NavMenu />
         <Layout>
-<<<<<<< HEAD
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path={MARKETPLACE_PATH} element={<Marketplace />} />
-            <Route path={NOTIFICATIONS_PATH} element={<Notification />} />
-            <Route path={PROFILE_PATH} element={<Profile />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-=======
           <Suspense fallback={<p>loading...</p>}>
             <Routes>
               <Route exact path="/" element={<Home />} />
               <Route path="/home" element={<Home />} />
+              <Route path={MARKETPLACE_PATH} element={<Marketplace />} />
               <Route path={NOTIFICATIONS_PATH} element={<Notification />} />
               <Route path={PROFILE_PATH} element={<Profile />} />
               <Route path="/settings" element={<Settings />} />
               <Route path="*" element={<Navigate to="/" />} />
             </Routes>
           </Suspense>
->>>>>>> 9752f7294776129a51571137cc0cf33a244f0c8e
         </Layout>
       </Router>
     </>
