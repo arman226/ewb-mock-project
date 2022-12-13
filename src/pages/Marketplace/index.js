1058964a9fd6dbb9752daa51162058ae005e138d
Randy Velasco <rbvelasco@eastwestbanker.com>
2022-12-13 15:23:57 +0800

    add marketplace on routes

--- src/pages/Marketplace/index.js
+++ src/pages/Marketplace/index.js
@@ -5,23 +5,10 @@ const Filter = lazy(() => import("./Filter" ));
 const Search = lazy(() => import("./Search" ));
 
 const Marketplace = () => {
-<<<<<<< HEAD
-   return (
-    <Suspense fallback="<>loading...<>">
-      <div className="container">
-        <Search/>
-        <br/>
-        <Filter/>
-        <br/>
-        <ProductListing />
-      </div>
-    </Suspense>
-=======
   return (
     <div>
       <ProductListing />
     </div>
->>>>>>> b7d65dde4fb37bcc01cbfbf95890953b549c774b
   );
 };
 
